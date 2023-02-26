567dd764e287d1f776aab5749d389b48dbf305fa
Tantowi Putra Agung Setiawan <tantowi6758@gmail.com>
2023-02-26 22:48:12 +0700

    Finishing GET API method for Posts

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::post('/register', [RegisterController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
